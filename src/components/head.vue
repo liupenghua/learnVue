274a82f67c9c4132a26ad813668df614724d9152
liupenghua <770605429@qq.com>
2017-08-21 14:30:39 +0800

    修改headpart样式

--- src/components/head.vue
+++ src/components/head.vue
@@ -4,12 +4,14 @@
 			<a href="#">
 			<img src='../assets/cnodejs.svg' title='cnodejs.svg'>
 			</a>
-			<span>首页</span>
-			<span>新手入门</span>
-			<span>API</span>
-			<span>关于</span>
-			<span>注册</span>
-			<span>登陆</span>			
+			<div class="head-right">
+				<span>首页</span>
+				<span>新手入门</span>
+				<span>API</span>
+				<span>关于</span>
+				<span>注册</span>
+				<span>登陆</span>	
+			</div>			
 		</div>
 	</div>
 </template>
@@ -29,22 +31,27 @@ export default{
 		height: 50px;
 		background-color: #444; 
 	}
-	div.head div{
-		height: 50px;
+	div.head >div{
+		height: 40px;
 		width: 1500px;
 		margin: 0px auto;
+		padding: 5px 0;
 	}
-	div.head div img{
-		height: 50px;
+	div.head div a img{
+		height: 40px;
 		width: 150px;
 		float: left;
 	}
-	div.head div span{
+	div.head-right {
 		display: inline-block;
+		width: 360px;
+		height: 40px;
 		float: right;
-		width: 80px;
-		height: 30px;
-		margin-top: 20px; 
+		text-align: center;
+	}
+	div.head-right span{
+		display: inline-block;
+		margin: 10px ; 
 		color: #ccc;
 		font-size: 14px;
 		cursor: pointer;
